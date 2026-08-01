f2d1adaaf66dfe492ac1382f5746c95d42feac71
Henrique Carvalho <iquecarvalho2005@gmail.com>
2026-08-01 09:56:35 -0300

    Time Manager

--- PGJ/Assets/Scripts/TimeManager.cs
+++ PGJ/Assets/Scripts/TimeManager.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TimeManager : MonoBehaviour
 {
+    [Header("Variables")]
     [SerializeField] private int startingHour;
     [SerializeField] private int endingHour;
     [SerializeField] private int stepDurationMinute;
     [SerializeField] private float tickDuration;
 
+    [Header("Events")]
+    [SerializeField] private UnityEvent onTickEvent;
+    [SerializeField] private UnityEvent onTimeEndEvent;
+
     private int currentHour = 0;
     private int currentMinute = 0;
     private float currentTickTime = 0;
+
+    private bool completed = false;
 
     private void Start()
     {
@@ -20,11 +28,7 @@ public class TimeManager : MonoBehaviour
 
     private void Update()
     {
-        currentTickTime += Time.deltaTime;
-        if (currentTickTime >= tickDuration) 
-        {
-            OnTick();
-        }
+        TickTimer();
     }
 
     public string GetTime() 
@@ -32,11 +36,35 @@ public class TimeManager : MonoBehaviour
         return $"{currentHour:D2}:{currentMinute:D2}";
     }
 
+    private void TickTimer() 
+    {
+        if (completed) return;
+
+        currentTickTime += Time.deltaTime;
+        if (currentTickTime >= tickDuration)
+        {
+            OnTick();
+        }
+    }
+
     private void OnTick() 
     {
+        if (completed) return;
+
         currentTickTime %= tickDuration;
         currentMinute += stepDurationMinute;
         currentHour += currentMinute / 60;
         currentMinute %= 60;
+
+        onTickEvent.Invoke();
+
+        if (currentHour >= endingHour)
+        {
+            currentHour = endingHour;
+            currentMinute = 0;
+            completed = true;
+
+            onTimeEndEvent.Invoke();
+        }
     }
 }
