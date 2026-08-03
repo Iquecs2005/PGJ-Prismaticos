13af2b5f2f27ceca5eff5e7bc1933431553f8106
JoaoAraujo <jpccaraujo00@gmail.com>
2026-08-03 11:40:41 -0300

    Damage Flash

---
+++ PGJ/Assets/Scripts/Entities/Flash/SimpleFlash.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
+
+namespace BarthaSzabolcs.Tutorial_SpriteFlash
+{
+    public class SimpleFlash : MonoBehaviour
+    {
+        #region Datamembers
+
+        #region Editor Settings
+
+        [Tooltip("Material to switch to during the flash.")]
+        [SerializeField] private Material flashMaterial;
+
+        [Tooltip("Duration of the flash.")]
+        [SerializeField] private float duration;
+
+        [SerializeField] private SpriteRenderer spriteRenderer;
+
+        #endregion
+
+        #region Private Fields
+
+        // The material that was in use, when the script started.
+        private Material originalMaterial;
+
+        // The currently running coroutine.
+        private Coroutine flashRoutine;
+
+        #endregion
+
+        #endregion
+
+        #region Methods
+
+        #region Unity Callbacks
+        void Start()
+        {
+            if (spriteRenderer == null)
+                spriteRenderer = GetComponent<SpriteRenderer>();
+
+            if (spriteRenderer == null)
+                spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+
+            if (spriteRenderer == null)
+            {
+                Debug.LogWarning($"SimpleFlash em '{name}': nenhum SpriteRenderer encontrado (nem nos filhos). Flash desativado.");
+                enabled = false;
+                return;
+            }
+
+            // Get the material that the SpriteRenderer uses,
+            // so we can switch back to it after the flash ended.
+            originalMaterial = spriteRenderer.material;
+        }
+        #endregion
+
+        public void Flash()
+        {
+            if (spriteRenderer == null)
+                return;
+
+            // If the flashRoutine is not null, then it is currently running.
+            if (flashRoutine != null)
+            {
+                // In this case, we should stop it first.
+                // Multiple FlashRoutines the same time would cause bugs.
+                StopCoroutine(flashRoutine);
+            }
+
+            // Start the Coroutine, and store the reference for it.
+            flashRoutine = StartCoroutine(FlashRoutine());
+        }
+
+        private IEnumerator FlashRoutine()
+        {
+            // Swap to the flashMaterial.
+            spriteRenderer.material = flashMaterial;
+
+            // Pause the execution of this function for "duration" seconds.
+            yield return new WaitForSeconds(duration);
+
+            // After the pause, swap back to the original material.
+            spriteRenderer.material = originalMaterial;
+
+            // Set the routine to null, signaling that it's finished.
+            flashRoutine = null;
+        }
+
+        #endregion
+    }
+}
