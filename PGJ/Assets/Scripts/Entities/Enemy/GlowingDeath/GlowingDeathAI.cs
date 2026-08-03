803c39bc6536dfdd2f5ba708507cf9cc435a09d4
Henrique Carvalho <iquecarvalho2005@gmail.com>
2026-08-03 00:58:49 -0300

    GlowingDeath

---
+++ PGJ/Assets/Scripts/Entities/Enemy/GlowingDeath/GlowingDeathAI.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowingDeathAI : BaseAI
+{
+    private GameObject prey;
+
+    protected override void OnChasing()
+    {
+        prey = GameManager.playerController.gameObject;
+    }
+
+    protected override void ChaseUpdate()
+    {
+        Vector2 dir = prey.transform.position - transform.position;
+        controller.movement.SetMoveInput(dir);
+    }
+}
